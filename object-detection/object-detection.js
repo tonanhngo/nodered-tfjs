0e304eb88c58f9ee38ed7454f76b42b4288ddc4b
Dave Conway-Jones <conway@uk.ibm.com>
2019-11-09 13:48:58 +0000

    Tidy up object detection node
    to be more "Node-RED" like :-)
    no changes in functionality (apart from checking that model exists before processing image)

--- object-detection/object-detection.js
+++ object-detection/object-detection.js
@@ -14,15 +14,17 @@ module.exports = function(RED) {
     cocoSsd.load({modelUrl: node.modelUrl}).then(model => {
       node.loadedModel = model;
       node.status({fill:'green', shape:'dot', text:'Model is ready'});
-      console.log('Object Detection Model Loaded.');
+      node.log('Object Detection Model Loaded.');
     });
 
     node.on('input', function(msg) {
-      const imgTensor = tf.node.decodeImage(new Uint8Array(msg.payload), channels = 3);
-      node.loadedModel.detect(imgTensor).then(predictions => {
-        msg.payload = predictions;
-        node.send(msg);
-      });
+      if (node.loadedModel) {
+        const imgTensor = tf.node.decodeImage(new Uint8Array(msg.payload), channels = 3);
+        node.loadedModel.detect(imgTensor).then(predictions => {
+          msg.payload = predictions;
+          node.send(msg);
+        });
+      }
     });
   }
   RED.nodes.registerType("object-detection", ObjectDetectionNode);
